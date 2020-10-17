6e8772857e3ef55868c371ec76ed53f25e4432a6
reisrb <rafael.reis@bandtec.com.br>
2020-10-17 20:00:55 -0300

    Criando repositorios do projeto

---
+++ catrina-c2/src/main/java/br/com/bandtec/catrinac2/repositorios/FabricanteRepository.java
@@ -0,0 +1,7 @@
+package br.com.bandtec.catrinac2.repositorios;
+
+import br.com.bandtec.catrinac2.dominios.Fabricante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FabricanteRepository extends JpaRepository<Fabricante, Integer> {
+}
---
+++ catrina-c2/src/main/java/br/com/bandtec/catrinac2/repositorios/RoboRepository.java
@@ -0,0 +1,7 @@
+package br.com.bandtec.catrinac2.repositorios;
+
+import br.com.bandtec.catrinac2.dominios.Robo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoboRepository extends JpaRepository<Robo, Integer> {
+}
