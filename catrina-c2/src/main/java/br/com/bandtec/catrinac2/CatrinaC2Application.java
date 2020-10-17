d7322064af9f5dfab09bfe5a76b602165d0c5b69
reisrb <rafael.reis@bandtec.com.br>
2020-10-17 19:27:04 -0300

    Criando projeto spring

---
+++ catrina-c2/src/main/java/br/com/bandtec/catrinac2/CatrinaC2Application.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.catrinac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatrinaC2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatrinaC2Application.class, args);
+	}
+
+}
