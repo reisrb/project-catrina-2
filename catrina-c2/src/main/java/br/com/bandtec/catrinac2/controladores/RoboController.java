395149bf6de247e93f15d4d01d4454f200b0544b
reisrb <rafael.reis@bandtec.com.br>
2020-10-18 00:53:19 -0300

    adicionando edição e busca por id

--- catrina-c2/src/main/java/br/com/bandtec/catrinac2/controladores/RoboController.java
+++ catrina-c2/src/main/java/br/com/bandtec/catrinac2/controladores/RoboController.java
@@ -1,9 +1,11 @@
 package br.com.bandtec.catrinac2.controladores;
 
+import br.com.bandtec.catrinac2.dominios.Fabricante;
 import br.com.bandtec.catrinac2.dominios.Robo;
 import br.com.bandtec.catrinac2.repositorios.FabricanteRepository;
 import br.com.bandtec.catrinac2.repositorios.RoboRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,18 +19,26 @@ public class RoboController {
   @Autowired
   private RoboRepository roboRepository;
 
-  @Autowired
-  private FabricanteRepository fabricanteRepository;
-
   @GetMapping
-  public ResponseEntity listar(){
+  public ResponseEntity listar() {
     List<Robo> listBd = roboRepository.findAll();
 
-    if(listBd.isEmpty()){
+    if (listBd.isEmpty()) {
       return ResponseEntity.noContent().build();
     }
     return ResponseEntity.ok(listBd);
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity busca(@PathVariable Integer id){
+    if(roboRepository.existsById(id)){
+      Optional<Robo> optionalRobo = roboRepository.findById(id);
+      return ResponseEntity.ok(optionalRobo);
+    }
+
+    return ResponseEntity.notFound().build();
+  }
+
 
   @PostMapping
   public ResponseEntity criar(@RequestBody Robo robo) {
@@ -45,16 +55,14 @@ public class RoboController {
 
   }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity deletar(@PathVariable Integer id) {
-    Optional<Robo> optionalRobo = roboRepository.findById(id);
-
-    if (optionalRobo.isPresent()){
-      Robo robo = optionalRobo.get();
-      roboRepository.delete(robo);
+  @PutMapping("/{id}")
+  public ResponseEntity<Void> atualizar(@PathVariable Integer id, @RequestBody Robo robo) {
+    if (roboRepository.existsById(id)) {
+      robo.setId(id);
+      roboRepository.save(robo);
       return ResponseEntity.ok().build();
     }
-    return ResponseEntity.of(optionalRobo);
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
   }
 
 }
