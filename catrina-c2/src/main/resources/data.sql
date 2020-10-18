4e8818e96707705caefbbaaf456a4d70d961d9f9
reisrb <rafael.reis@bandtec.com.br>
2020-10-18 00:52:36 -0300

    Alteração em um insert

---
+++ catrina-c2/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into pais(id, nome) values
+(null, 'Italia'),
+(null, 'Estados Unidos'),
+(null, 'Japão');
+
+insert into fabricante(id, nome, pais_id) values
+(null, 'Machine AI', 3);
+
+insert into robo(id, nome, modelo, capacidade_bateria, fabricante_id) values
+(null, 'Marvin', 'ArchTwo', 4200, 1);
